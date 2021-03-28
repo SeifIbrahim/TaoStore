21240cabbd46709aaf157acfe2368949d150e3e2
Seif Ibrahim <seifibrahim10@gmail.com>
2021-03-28 14:16:49 -0700

    add compile commands

---
+++ compile-commands.sh
@@ -0,0 +1,7 @@
+# bin/CockroachTaoProxy.jar
+javac -d bin/ -cp src/:lib/commons-math3-3.6.1.jar:lib/guava-19.0.jar:lib/postgresql-42.2.18.jar -sourcepath src/ src/TaoProxy/CockroachTaoProxy.java
+(cd bin/ && jar cfe CockroachTaoProxy.jar TaoProxy.CockroachTaoProxy Configuration Messages TaoProxy)
+
+# bin/TaoClient.jar
+javac -d bin/ -cp src/:lib/commons-math3-3.6.1.jar:lib/guava-19.0.jar:lib/postgresql-42.2.18.jar -sourcepath src/ src/TaoClient/TaoClient.java
+(cd bin/ && jar cfe TaoClient.jar TaoClient.TaoClient Configuration Messages TaoClient)

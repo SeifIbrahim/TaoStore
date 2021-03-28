0d448f4c82ecd39116add52cc7cfa111aabdd3c8
Seif Ibrahim <seifibrahim10@gmail.com>
2021-03-28 16:05:16 -0700

    fix compile-commands and add run-client and run-proxy

--- compile-commands.sh
+++ compile-commands.sh
@@ -4,4 +4,4 @@ javac -d bin/ -cp src/:lib/commons-math3-3.6.1.jar:lib/guava-19.0.jar:lib/postgr
 
 # bin/TaoClient.jar
 javac -d bin/ -cp src/:lib/commons-math3-3.6.1.jar:lib/guava-19.0.jar:lib/postgresql-42.2.18.jar -sourcepath src/ src/TaoClient/TaoClient.java
-(cd bin/ && jar cfe TaoClient.jar TaoClient.TaoClient Configuration Messages TaoClient)
+(cd bin/ && jar cfe TaoClient.jar TaoClient.TaoClient Configuration Messages TaoClient TaoProxy)

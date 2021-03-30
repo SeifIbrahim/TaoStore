5b0629c8274c67c19af53719b4bc747ea8aa0379
Seif Ibrahim <seifibrahim10@gmail.com>
2021-03-29 18:37:54 -0700

    update run-client

--- scripts/run-client.sh
+++ scripts/run-client.sh
@@ -1,1 +1,1 @@
-java -cp bin/TaoClient.jar:lib/* TaoClient.TaoClient --runType test --load_test_type multiclient --clients 10 --zipfExp 0.9 --rwRatio 0.5 --load_size 1000
+java -cp bin/TaoClient.jar:lib/* TaoClient.TaoClient --runType test --load_test_type multiclient --clients $1 --load_size $2 --rwRatio $3 --zipfExp $4
